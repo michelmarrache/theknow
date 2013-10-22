6ae026d1e81c67b7de14e386a081f3d906ae90e3
Juan Lopez <onel0p3z@gmail.com>
2013-10-22 01:27:44 -0400

    Fixed /directory rendering problem, added .gitignore, check db config b4 running

--- app/routes/directory.js
+++ app/routes/directory.js
@@ -3,14 +3,32 @@ var Auth = require('../../config/middlewares/authorization.js');
 
 module.exports = function(app, passport){
   app.get('/directory', Auth.isAuthenticated , function(req, res){
-    User.find({},{}, function (err, users) {
-      var userMap = {};
+    User.find({},{}, function (err, users){
+      // Console statments are your best friends :D
+      //console.log('users:---->', users);
+      /*
+      //This is not necessary as mongoose returns an array
+      //of objects containing all users ... not sure if that's
+      //what you want but it works.
+      var userMap = [];
       users.forEach(function(user) {
+        console.log('user in forEach:--->', user)
         userMap[user.email] = user;
-      })
-      console.log(userMap);
-    res.render('directory', { userMap : userMap });
-    })
+      });
+      console.log('users!!! ----->',users);
+      */
+      ////////////////////////////////////////////////////////
+      /*
+      * This is your big error.
+      * From layout.jade, you are rendering with a variable
+      * of user and you were displaying this in the top bar
+      * layout.jade at line 53 ;)
+      */
+      var user = {firstName: req.user.firstName };
+
+      // Now just add it to the object passed to jade engine
+      res.render('directory', { 'userMap' : users, 'user':user });
+    });
   });
 
   app.get('/edituser/:email', Auth.isAuthenticated , function(req, res){
